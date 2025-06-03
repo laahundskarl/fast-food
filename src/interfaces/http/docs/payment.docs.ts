d43325c9c2677cd93b4e8045c2eeb07612bc81ee
Fabio Smaniotto Bernardes <fabio.bernardes@gft.com>
2025-06-03 13:16:39 -0300

    feat: swagger payment and collection

--- src/interfaces/http/docs/payment.docs.ts
+++ src/interfaces/http/docs/payment.docs.ts
@@ -1,7 +1,21 @@
+import z from 'zod';
+
+import { errorNotFoundSchema, errorResponseValidationSchema } from '#/interfaces/http/docs/util.docs';
+import { paymentResponseSchema } from '#/interfaces/http/schema/payment.schema';
+
 export const paymentGetSchema = {
     schema: {
         tags: ['Pagamentos'],
         summary: 'Busca pagamento',
+        query: z.object({
+            name: z.string().optional(),
+            categoryId: z.string().uuid().optional(),
+            productId: z.string().uuid().optional(),
+        }),
+        response: {
+            // 200: paymentResponseSchema,
+            404: errorNotFoundSchema,
+        },
     },
 };
 
@@ -9,6 +23,14 @@ export const paymentListSchema = {
     schema: {
         tags: ['Pagamentos'],
         summary: 'Lista pagamentos',
+        params: z.object({
+            orderId: z.string().optional(),
+            status: z.string().optional(),
+        }),
+        response: {
+            200: z.array(paymentResponseSchema),
+            404: errorNotFoundSchema,
+        },
     },
 };
 
@@ -16,5 +38,19 @@ export const paymentUpdateSchema = {
     schema: {
         tags: ['Pagamentos'],
         summary: 'Atualiza pagamento',
+        body: z.object({
+            name: z.string().optional(),
+            description: z.string().optional(),
+            value: z.number().optional(),
+            categoryId: z.string().uuid().optional(),
+        }),
+        params: z.object({
+            id: z.string().uuid(),
+        }),
+        response: {
+            200: paymentResponseSchema,
+            404: errorNotFoundSchema,
+            400: errorResponseValidationSchema,
+        },
     },
 };
--- src/interfaces/http/schema/payment.schema.ts
+++ src/interfaces/http/schema/payment.schema.ts
@@ -2,7 +2,19 @@ import z from 'zod';
 
 export const paymentResponseSchema = z.object({
     id: z.string().uuid(),
+    orderId: z.string().optional(),
     status: z.string(),
     externalReference: z.string().nullable(),
     qrCode: z.string().nullable(),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+    orders: z
+        .object({
+            id: z.string().uuid().optional(),
+            value: z.number(),
+            orderNumber: z.number(),
+            status: z.string(),
+            clientId: z.string().uuid(),
+        })
+        .optional(),
 });
