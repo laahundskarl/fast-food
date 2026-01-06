b50b8a899d8a3a58032a05db86962d7453c3e895
Gabriel Duarte <gabrielgsd03@gmail.com>
2026-01-05 21:01:41 -0300

    vitest: added vitest config with coverage set up

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+const resolveAliases = {
+  '#': path.resolve(__dirname, 'src'),
+}
+
+export default defineConfig({
+  resolve: {
+    alias: resolveAliases,
+  },
+  test: {
+    coverage: {
+      thresholds: {
+        statements: 70,
+        functions: 70,
+        branches: 40,
+        lines: 70,
+      },
+      reportOnFailure: true,
+    },
+    globals: true,
+    environment: 'node',
+    root: './src',
+    testTimeout: 10000,
+  },
+});
